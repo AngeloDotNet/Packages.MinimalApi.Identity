dcba98f71a14c45f6bd1567975d688332bf60a7d
Angelo Pirola <angelo9871@gmail.com>
2025-02-28 00:02:26 +0100

    Refactor entity classes and remove unused properties
    Removed several properties and collections from entity classes related to user roles, permissions, licenses, and modules in the Minimal API Identity project. This includes the removal of `FirstName`, `LastName`, `UserRoles`, `UserLicenses`, and `UserModules` from relevant classes. The `MinimalApiDbContext` class has also been updated to remove entity relationship configurations and seed data for permissions, indicating a simplification of the data model. Additionally, a project reference was removed from the `.csproj` file, suggesting a restructuring of project dependencies.

--- src/MinimalApi.Identity.DataAccessLayer/Entities/ApplicationUserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace MinimalApi.Identity.DataAccessLayer.Entities;
-
-public class ApplicationUserRole : IdentityUserRole<int>
-{
-    public ApplicationUser User { get; set; } = null!;
-    public ApplicationRole Role { get; set; } = null!;
-}
--- src/MinimalApi.Identity.DataAccessLayer/Entities/Module.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MinimalApi.Identity.DataAccessLayer.Entities;
-
-public class Module
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
-    public ICollection<UserModule> UserModules { get; set; } = [];
-}
