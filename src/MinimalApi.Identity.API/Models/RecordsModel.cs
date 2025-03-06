d89bfc5901327aff507313f267dcc6e932c5e122
Angelo Pirola <angelo9871@gmail.com>
2025-03-06 08:57:13 +0100

    Add ChangeEmailModel for email update functionality
    Introduced a new record class `ChangeEmailModel` with properties `Email` and `NewEmail`. This model facilitates the process of changing a user's email address.

--- src/MinimalApi.Identity.API/Models/RecordsModel.cs
+++ src/MinimalApi.Identity.API/Models/RecordsModel.cs
@@ -5,6 +5,7 @@ public record class LoginModel(string Username, string Password, bool RememberMe
 public record class AuthResponse(string AccessToken, string RefreshToken, DateTime ExpiredToken);
 public record class ForgotPasswordModel(string Email);
 public record class ResetPasswordModel(string Email, string Token, string Password);
+public record class ChangeEmailModel(string Email, string NewEmail);
 public record class UserProfileModel(string Username, string Email, string FirstName, string LastName)
 {
     public string FullName => $"{FirstName} {LastName}";
