8beafb52c2bd93930394e43e011beb41272c8bd6
Angelo Pirola <angelo9871@gmail.com>
2025-03-07 00:19:54 +0100

    Consolidate permission handling into CombinedPermissionHandler
    This commit introduces a new class, `CombinedPermissionHandler`, which implements the `IAuthorizationHandler` interface. The previous `MultiPermissionHandler` and `PermissionHandler` classes have been removed. The new handler efficiently processes multiple authorization requirements by checking user claims against specified permissions, simplifying the permission handling logic in the application.

---
+++ src/MinimalApi.Identity.API/Authorization/Handlers/CombinedPermissionHandler.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using MinimalApi.Identity.API.Extensions;
+using MinimalApi.Identity.BusinessLayer.Authorization.Requirement;
+
+namespace MinimalApi.Identity.API.Authorization.Handlers;
+
+public class CombinedPermissionHandler : IAuthorizationHandler
+{
+    public async Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        var confirmedPolicies = true;
+
+        foreach (var requirement in context.Requirements)
+        {
+            if (requirement is MultiPolicyRequirement multiPolicyRequirement)
+            {
+                if (context.User.Identity!.IsAuthenticated == true)
+                {
+                    foreach (var permission in multiPolicyRequirement.Policies)
+                    {
+                        if (!context.User.Claims.Any(claim => claim.Type == CustomClaimTypes.Permission && claim.Value == permission))
+                        {
+                            confirmedPolicies = false;
+                            break;
+                        }
+                    }
+
+                    if (confirmedPolicies)
+                    {
+                        context.Succeed(multiPolicyRequirement);
+                    }
+                }
+            }
+            else if (requirement is AuthorizationRequirement authorizationRequirement)
+            {
+                var permission = authorizationRequirement.Permission;
+
+                if (context.Resource is HttpContext httpContext)
+                {
+                    if (context.User.Identity!.IsAuthenticated == true && !string.IsNullOrWhiteSpace(permission))
+                    {
+                        if (!context.User.Claims.Any(claim => claim.Type == CustomClaimTypes.Permission && claim.Value == permission))
+                        {
+                            context.Fail();
+                        }
+                        else
+                        {
+                            context.Succeed(authorizationRequirement);
+                        }
+                    }
+                }
+            }
+        }
+
+        await Task.CompletedTask;
+    }
+}
