e36f8e2963e85691182a5a55b370dcde78675117
Angelo Pirola <angelo9871@gmail.com>
2025-03-06 08:57:02 +0100

    Add GetPermissionsFromUserAsync method structure
    - Added a commented-out method `GetPermissionsFromUserAsync` in `AuthService.cs` for future implementation to retrieve user permissions.
    - Updated `IAuthService.cs` to include a new method signature for `GetPermissionsFromUserAsync(ApplicationUser user)` and added a `using` directive for `MinimalApi.Identity.API.Entities`.

--- src/MinimalApi.Identity.API/Services/AuthService.cs
+++ src/MinimalApi.Identity.API/Services/AuthService.cs
@@ -20,4 +20,15 @@ public class AuthService(MinimalApiDbContext dbContext) : IAuthService
 
         return permissions;
     }
+
+    //public async Task<IList<string>> GetPermissionsFromUserAsync(ApplicationUser user)
+    //{
+    //    var permissions = await dbContext.UserPermissions
+    //        .Where(up => up.UserId == user.Id)
+    //        .Select(up => up.Permission.Name)
+    //        .Distinct()
+    //        .ToListAsync();
+
+    //    return permissions;
+    //}
 }
--- src/MinimalApi.Identity.API/Services/Interfaces/IAuthService.cs
+++ src/MinimalApi.Identity.API/Services/Interfaces/IAuthService.cs
@@ -1,6 +1,9 @@
-﻿namespace MinimalApi.Identity.API.Services.Interfaces;
+﻿using MinimalApi.Identity.API.Entities;
+
+namespace MinimalApi.Identity.API.Services.Interfaces;
 
 public interface IAuthService
 {
+    Task<IList<string>> GetPermissionsFromUserAsync(ApplicationUser user);
     Task<IList<string>> GetPermissionsFromRolesAsync(IList<string> roles);
 }
