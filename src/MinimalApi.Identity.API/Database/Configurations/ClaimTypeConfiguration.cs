80161905f203dd010890eb6340c1e4eff82df782
Angelo Pirola <angelo9871@gmail.com>
2025-03-15 00:44:47 +0100

    Add ClaimType configuration and remove Permissions
    This commit introduces the `ClaimTypeConfiguration` class to configure the `ClaimType` entity with predefined data. The `PermissionConfiguration` and `UserPermissionConfiguration` classes have been removed, along with their corresponding `DbSet` properties in `MinimalApiDbContext`. The `ClaimType` entity is now included in the context.

---
+++ src/MinimalApi.Identity.API/Database/Configurations/ClaimTypeConfiguration.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MinimalApi.Identity.API.Entities;
+using MinimalApi.Identity.API.Enums;
+
+namespace MinimalApi.Identity.API.Database.Configurations;
+
+public class ClaimTypeConfiguration : IEntityTypeConfiguration<ClaimType>
+{
+    public void Configure(EntityTypeBuilder<ClaimType> builder)
+    {
+        builder.HasData(new ClaimType
+        {
+            Id = 1,
+            Type = "Permission",
+            Value = nameof(Permissions.Profilo),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 2,
+            Type = "Permission",
+            Value = nameof(Permissions.ProfiloRead),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 3,
+            Type = "Permission",
+            Value = nameof(Permissions.ProfiloWrite),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 4,
+            Type = "Permission",
+            Value = nameof(Permissions.Ruolo),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 5,
+            Type = "Permission",
+            Value = nameof(Permissions.RuoloRead),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 6,
+            Type = "Permission",
+            Value = nameof(Permissions.RuoloWrite),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 7,
+            Type = "Permission",
+            Value = nameof(Permissions.Permesso),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 8,
+            Type = "Permission",
+            Value = nameof(Permissions.PermessoRead),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 9,
+            Type = "Permission",
+            Value = nameof(Permissions.PermessoWrite),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 10,
+            Type = "Permission",
+            Value = nameof(Permissions.Modulo),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 11,
+            Type = "Permission",
+            Value = nameof(Permissions.ModuloRead),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 12,
+            Type = "Permission",
+            Value = nameof(Permissions.ModuloWrite),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 13,
+            Type = "Permission",
+            Value = nameof(Permissions.Licenza),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 14,
+            Type = "Permission",
+            Value = nameof(Permissions.LicenzaRead),
+            Default = true
+        },
+        new ClaimType
+        {
+            Id = 15,
+            Type = "Permission",
+            Value = nameof(Permissions.LicenzaWrite),
+            Default = true
+        }
+        );
+    }
+}
--- src/MinimalApi.Identity.API/Database/MinimalApiDbContext.cs
+++ src/MinimalApi.Identity.API/Database/MinimalApiDbContext.cs
@@ -9,14 +9,13 @@ namespace MinimalApi.Identity.API.Database;
 public class MinimalApiDbContext(DbContextOptions<MinimalApiDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, int,
     IdentityUserClaim<int>, ApplicationUserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>(options)
 {
-    public DbSet<Permission> Permissions { get; set; }
-    public DbSet<UserPermission> UserPermissions { get; set; }
     public DbSet<License> Licenses { get; set; }
     public DbSet<UserLicense> UserLicenses { get; set; }
     public DbSet<Entities.Module> Modules { get; set; }
     public DbSet<UserModule> UserModules { get; set; }
     public DbSet<EmailSending> EmailSendings { get; set; }
     public DbSet<EmailSendingType> EmailSendingTypes { get; set; }
+    public DbSet<ClaimType> ClaimTypes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
