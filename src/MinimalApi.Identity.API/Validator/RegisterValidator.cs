f0efa8994b5185d32385773a3b14991d677102cd
Angelo Pirola <angelo9871@gmail.com>
2025-03-16 10:54:29 +0100

    Add validators for login and registration models
    Introduce `LoginValidator` and `RegisterValidator` classes using FluentValidation.
    The `LoginValidator` ensures `Username` and `Password` are not empty, while the
    `RegisterValidator` checks that `FirstName`, `LastName`, `Username`, `Email`,
    and `Password` are not empty, with email format validation included.
    Commented-out length validations are present but inactive.

---
+++ src/MinimalApi.Identity.API/Validator/RegisterValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MinimalApi.Identity.API.Models;
+
+namespace MinimalApi.Identity.API.Validator;
+
+public class RegisterValidator : AbstractValidator<RegisterModel>
+{
+    public RegisterValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            //.MaximumLength(50).WithMessage("First name must not exceed 50 characters")
+            ;
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            //.MaximumLength(50).WithMessage("Last name must not exceed 50 characters")
+            ;
+
+        RuleFor(x => x.Username)
+            .NotEmpty().WithMessage("Username is required")
+            //.MaximumLength(50).WithMessage("Username must not exceed 50 characters")
+            ;
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Email is not valid");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            //.MinimumLength(6).WithMessage("Password must be at least 6 characters")
+            ;
+    }
+}
