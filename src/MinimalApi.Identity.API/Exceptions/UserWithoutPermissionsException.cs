bef7bfc735ec25bf2bc95a562ded5b61a65b76cd
Angelo Pirola <angelo9871@gmail.com>
2025-03-16 10:55:46 +0100

    Add user-related exception classes
    Introduced `UserUnknownException` and `UserWithoutPermissionsException` in the `MinimalApi.Identity.API.Exceptions` namespace. Each class extends the base `Exception` class and provides three constructors for flexibility in exception handling.

---
+++ src/MinimalApi.Identity.API/Exceptions/UserWithoutPermissionsException.cs
@@ -0,0 +1,16 @@
+﻿namespace MinimalApi.Identity.API.Exceptions;
+
+public class UserWithoutPermissionsException : Exception
+{
+    public UserWithoutPermissionsException()
+    {
+    }
+
+    public UserWithoutPermissionsException(string? message) : base(message)
+    {
+    }
+
+    public UserWithoutPermissionsException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
