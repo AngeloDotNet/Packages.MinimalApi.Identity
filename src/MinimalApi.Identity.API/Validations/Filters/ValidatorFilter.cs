ad4452dd9abf1c4114973fa8c3acb347c0266034
Angelo Pirola <angelo9871@gmail.com>
2025-03-16 10:55:23 +0100

    Refactor validation error handling in ValidatorFilter
    Removed commented-out code for TypedResults.Problem response.
    Replaced it with a direct creation of a ProblemDetails object
    for validation errors, including status code 422 and relevant
    request information. Retained error formatting logic based
    on ErrorResponseFormat.

--- src/MinimalApi.Identity.API/Validations/Filters/ValidatorFilter.cs
+++ src/MinimalApi.Identity.API/Validations/Filters/ValidatorFilter.cs
@@ -28,19 +28,6 @@ internal class ValidatorFilter<T>(IValidator<T> validator, IOptions<ValidationOp
         var errors = validationResult.ToDictionary();
         var messageError = validationOptions.ValidationErrorTitleMessageFactory?.Invoke(context, errors) ?? "One or more validation errors occurred";
 
-        //var result = TypedResults.Problem(
-        //    statusCode: StatusCodes.Status422UnprocessableEntity,
-        //    instance: context.HttpContext.Request.Path,
-        //    title: validationOptions.ValidationErrorTitleMessageFactory?.Invoke(context, errors) ?? "One or more validation errors occurred",
-        //    extensions: new Dictionary<string, object?>(StringComparer.Ordinal)
-        //    {
-        //        ["traceId"] = Activity.Current?.Id ?? context.HttpContext.TraceIdentifier,
-        //        ["errors"] = validationOptions.ErrorResponseFormat == ErrorResponseFormat.Default
-        //            ? errors : errors.SelectMany(e
-        //                => e.Value.Select(m => new { Name = e.Key, Message = m })).ToArray()
-        //    }
-        //);
-
         var statusCode422 = StatusCodes.Status422UnprocessableEntity;
         var result = new ProblemDetails
         {
@@ -53,8 +40,6 @@ internal class ValidatorFilter<T>(IValidator<T> validator, IOptions<ValidationOp
             {
                 ["traceId"] = Activity.Current?.Id,
                 ["requestId"] = context.HttpContext.TraceIdentifier,
-                //["userId"] = context.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier),
-                //["userName"] = context.HttpContext.User?.FindFirstValue(ClaimTypes.Name),
                 ["dateTime"] = DateTime.UtcNow,
                 ["errors"] = validationOptions.ErrorResponseFormat == ErrorResponseFormat.Default ?
                 errors : errors.SelectMany(e
