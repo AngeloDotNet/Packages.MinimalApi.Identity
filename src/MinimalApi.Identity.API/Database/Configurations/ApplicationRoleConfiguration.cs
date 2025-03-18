1ae7eea54fa122f5a5422b1269ea8e14c9968df4
Angelo Pirola <angelo9871@gmail.com>
2025-03-19 00:59:32 +0100

    Update role configurations and add new roles
    Changed the `ConcurrencyStamp` for the `Admin` role.
    Added new roles: `PowerUser` and `User`, both with default settings.

--- src/MinimalApi.Identity.API/Database/Configurations/ApplicationRoleConfiguration.cs
+++ src/MinimalApi.Identity.API/Database/Configurations/ApplicationRoleConfiguration.cs
@@ -14,7 +14,23 @@ public class ApplicationRoleConfiguration : IEntityTypeConfiguration<Application
             Id = 1,
             Name = nameof(DefaultRoles.Admin),
             NormalizedName = nameof(DefaultRoles.Admin).ToUpper(),
-            ConcurrencyStamp = "52D77FEB-3860-4523-B022-4F5CB859E434",
+            ConcurrencyStamp = "2F267733-F53E-498F-B91E-C536BCE4AEA3",
+            Default = true
+        },
+        new ApplicationRole
+        {
+            Id = 2,
+            Name = nameof(DefaultRoles.PowerUser),
+            NormalizedName = nameof(DefaultRoles.PowerUser).ToUpper(),
+            ConcurrencyStamp = "36741E9D-5F55-4994-B9BE-F63F93A81EE0",
+            Default = true
+        },
+        new ApplicationRole
+        {
+            Id = 3,
+            Name = nameof(DefaultRoles.User),
+            NormalizedName = nameof(DefaultRoles.User).ToUpper(),
+            ConcurrencyStamp = "4535A9B1-B787-4CA6-ACAD-F2E0DF38AB5B",
             Default = true
         });
     }
