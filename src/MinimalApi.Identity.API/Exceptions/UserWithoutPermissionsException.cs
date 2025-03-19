a9417c7458291043253681d4c1278529465da843
Angelo Pirola <angelo9871@gmail.com>
2025-03-20 00:16:28 +0100

    Refactor exception classes and add validation exception
    Removed empty constructors from UserUnknownException and
    UserWithoutPermissionsException. Added ValidationModelException
    to handle validation errors with a message and a dictionary of
    error details.

--- src/MinimalApi.Identity.API/Exceptions/UserWithoutPermissionsException.cs
+++ src/MinimalApi.Identity.API/Exceptions/UserWithoutPermissionsException.cs
@@ -3,14 +3,11 @@
 public class UserWithoutPermissionsException : Exception
 {
     public UserWithoutPermissionsException()
-    {
-    }
+    { }
 
     public UserWithoutPermissionsException(string? message) : base(message)
-    {
-    }
+    { }
 
     public UserWithoutPermissionsException(string? message, Exception? innerException) : base(message, innerException)
-    {
-    }
+    { }
 }
---
+++ src/MinimalApi.Identity.API/Exceptions/ValidationModelException.cs
@@ -0,0 +1,6 @@
+﻿namespace MinimalApi.Identity.API.Exceptions;
+
+public class ValidationModelException(string message, IDictionary<string, string[]> errors) : Exception(message)
+{
+    public IDictionary<string, string[]> Errors { get; } = errors;
+}
