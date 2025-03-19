d1553615a7eaa1457add038adc958a4512ec10a7
Angelo Pirola <angelo9871@gmail.com>
2025-03-20 00:16:20 +0100

    Add UserProfile to ApplicationUser and refactor claims
    - Introduced `UserProfile` property in `ApplicationUser`.
    - Updated `UserClaims` to use `ICollection<IdentityUserClaim<int>>`.
    - Removed unused `ApplicationUserClaim` class.
    - Simplified `BaseEntity` by removing commented attribute.
    - Added new `UserProfile` class with relevant properties.

--- src/MinimalApi.Identity.API/Entities/Common/BaseEntity.cs
+++ src/MinimalApi.Identity.API/Entities/Common/BaseEntity.cs
@@ -2,6 +2,5 @@
 
 public class BaseEntity
 {
-    //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 }
---
+++ src/MinimalApi.Identity.API/Entities/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using MinimalApi.Identity.API.Entities.Common;
+
+namespace MinimalApi.Identity.API.Entities;
+
+public class UserProfile : BaseEntity
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    //public bool IsEnabled { get; set; }
+    //public DateTime? LastChangePassword { get; set; }
+    public int UserId { get; set; }
+    public ApplicationUser User { get; set; } = null!;
+}
