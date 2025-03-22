02a706f0005322381e08643cb273b7ca4587dd5f
Angelo Pirola <angelo9871@gmail.com>
2025-03-23 00:09:25 +0100

    Add ValidationOptions class for error response formatting
    This commit introduces a new class named `ValidationOptions` in the `MinimalApi.Identity.API.Options` namespace. The class includes a property `ErrorResponseFormat` that specifies the format for error responses.

---
+++ src/MinimalApi.Identity.API/Options/ValidationOptions.cs
@@ -0,0 +1,8 @@
+﻿using MinimalApi.Identity.API.Enums;
+
+namespace MinimalApi.Identity.API.Options;
+
+public class ValidationOptions
+{
+    public ErrorResponseFormat ErrorResponseFormat { get; set; }
+}
