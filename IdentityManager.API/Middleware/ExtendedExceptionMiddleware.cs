61de8e061ccb4840eb9feeef3fe787b9e410dddc
Angelo Pirola <angelo9871@gmail.com>
2025-03-25 00:00:47 +0100

    Add ExtendedExceptionMiddleware for error handling
    Introduces `ExtendedExceptionMiddleware` class to manage exceptions in the web application. Implements `HandleExceptionAsync` method to create and return a JSON-formatted error response with appropriate HTTP status codes. Adds necessary using directives for required namespaces.

---
+++ IdentityManager.API/Middleware/ExtendedExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MinimalApi.Identity.API.Middleware;
+using MinimalApi.Identity.API.Options;
+namespace IdentityManager.API.Middleware;
+
+public class ExtendedExceptionMiddleware(RequestDelegate next, IOptions<ValidationOptions> options)
+    : MinimalApiExceptionMiddleware(next, options)
+{
+    public static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        ProblemDetails problemDetails;
+
+        switch (exception)
+        {
+            //case CustomException customException:
+            //    problemDetails = CreateProblemDetails(context, HttpStatusCode.BadRequest, customException.Message);
+            //    problemDetails.Extensions["customProperty"] = customException.CustomProperty;
+            //    break;
+            default:
+                problemDetails = CreateProblemDetails(context, HttpStatusCode.InternalServerError, "An unexpected error occurred!");
+                problemDetails.Status = (int)HttpStatusCode.InternalServerError;
+                break;
+        }
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = problemDetails.Status ?? (int)HttpStatusCode.InternalServerError;
+
+        var json = JsonSerializer.Serialize(problemDetails);
+        return context.Response.WriteAsync(json);
+    }
+}
