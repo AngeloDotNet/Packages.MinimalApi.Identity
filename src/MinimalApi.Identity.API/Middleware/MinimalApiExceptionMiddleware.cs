e22e7f1cbc8325507e1a3d2873083995fb51082c
Angelo Pirola <angelo9871@gmail.com>
2025-03-26 11:28:31 +0100

    Enhance MinimalApiExceptionMiddleware for validation
    Updated MinimalApiExceptionMiddleware to accept IOptions<ValidationOptions> for improved error handling. Added specific exception cases for better ProblemDetails creation and enhanced ValidationModelException handling based on ErrorResponseFormat. Included stack trace in development mode and optimized user claims processing using a dictionary.

--- src/MinimalApi.Identity.API/Middleware/MinimalApiExceptionMiddleware.cs
+++ src/MinimalApi.Identity.API/Middleware/MinimalApiExceptionMiddleware.cs
@@ -1,17 +1,25 @@
 ﻿using System.Net;
 using System.Security.Claims;
 using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Http.Features.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using MinimalApi.Identity.API.Constants;
+using MinimalApi.Identity.API.Enums;
 using MinimalApi.Identity.API.Exceptions;
+using MinimalApi.Identity.API.Options;
 
 namespace MinimalApi.Identity.API.Middleware;
 
-public class MinimalApiExceptionMiddleware(RequestDelegate next)
+public class MinimalApiExceptionMiddleware(RequestDelegate next, IOptions<ValidationOptions> options)
 {
+    private readonly ValidationOptions validationOptions = options.Value;
+
     public async Task InvokeAsync(HttpContext httpContext)
     {
         try
@@ -20,16 +28,32 @@ public class MinimalApiExceptionMiddleware(RequestDelegate next)
         }
         catch (Exception ex)
         {
-            await HandleExceptionAsync(httpContext, ex);
+            await HandleExceptionAsync(httpContext, ex, validationOptions);
         }
     }
 
-    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception, ValidationOptions validationOptions)
     {
         ProblemDetails problemDetails;
 
         switch (exception)
         {
+            case BadRequestProfileException badRequestProfileException:
+                problemDetails = CreateProblemDetails(context, HttpStatusCode.BadRequest, badRequestProfileException.Message);
+                problemDetails.Status = (int)HttpStatusCode.BadRequest;
+                break;
+            case NotFoundProfileException notFoundProfileException:
+                problemDetails = CreateProblemDetails(context, HttpStatusCode.NotFound, notFoundProfileException.Message);
+                problemDetails.Status = (int)HttpStatusCode.NotFound;
+                break;
+            case ArgumentOutOfRangeException argumentOutOfRangeException:
+                problemDetails = CreateProblemDetails(context, HttpStatusCode.BadRequest, argumentOutOfRangeException.Message);
+                problemDetails.Status = (int)HttpStatusCode.BadRequest;
+                break;
+            case ArgumentNullException argumentNullException:
+                problemDetails = CreateProblemDetails(context, HttpStatusCode.BadRequest, argumentNullException.Message);
+                problemDetails.Status = (int)HttpStatusCode.BadRequest;
+                break;
             case UserUnknownException userUnknownException:
                 problemDetails = CreateProblemDetails(context, HttpStatusCode.Unauthorized, MessageApi.UserNotAuthenticated);
                 problemDetails.Status = (int)HttpStatusCode.Unauthorized;
@@ -41,8 +65,29 @@ public class MinimalApiExceptionMiddleware(RequestDelegate next)
             case ValidationModelException validationException:
                 problemDetails = CreateProblemDetails(context, HttpStatusCode.UnprocessableEntity, validationException.Message);
                 problemDetails.Status = (int)HttpStatusCode.UnprocessableEntity;
-                problemDetails.Extensions["errors"] = validationException.Errors
-                    .SelectMany(e => e.Value.Select(m => new { Name = e.Key, Message = m })).ToArray();
+
+                //if (validationOptions.ErrorResponseFormat == ErrorResponseFormat.Default)
+                //{
+                //    problemDetails.Extensions["errors"] = validationException.Errors;
+                //}
+                //else if (validationOptions.ErrorResponseFormat == ErrorResponseFormat.List)
+                //{
+                //    problemDetails.Extensions["errors"] = validationException.Errors.SelectMany(e
+                //        => e.Value.Select(m => new { Name = e.Key, Message = m })).ToArray();
+                //}
+
+                switch (validationOptions.ErrorResponseFormat)
+                {
+                    case ErrorResponseFormat.Default:
+                        problemDetails.Extensions["errors"] = validationException.Errors;
+                        break;
+                    case ErrorResponseFormat.List:
+                        problemDetails.Extensions["errors"] = validationException.Errors
+                            .SelectMany(e => e.Value.Select(m => new { Name = e.Key, Message = m }))
+                            .ToArray();
+                        break;
+                }
+
                 break;
             default:
                 problemDetails = CreateProblemDetails(context, HttpStatusCode.InternalServerError, MessageApi.UnexpectedError);
@@ -82,10 +127,29 @@ public class MinimalApiExceptionMiddleware(RequestDelegate next)
             }
         };
 
+        if (context.RequestServices.GetRequiredService<IHostEnvironment>().IsDevelopment())
+        {
+            var stackTrace = context.Features.Get<IExceptionHandlerFeature>()?.Error?.StackTrace;
+
+            if (!string.IsNullOrEmpty(stackTrace))
+            {
+                problemDetails.Extensions["stackTrace"] = stackTrace;
+            }
+        }
+
         if (user?.Identity?.IsAuthenticated == true)
         {
-            problemDetails.Extensions["userId"] = user.FindFirstValue(ClaimTypes.NameIdentifier);
-            problemDetails.Extensions["userName"] = user.FindFirstValue(ClaimTypes.Name);
+            var claims = user.Claims.ToDictionary(c => c.Type, c => c.Value);
+
+            if (claims.TryGetValue(ClaimTypes.NameIdentifier, out var userId))
+            {
+                problemDetails.Extensions["userId"] = userId;
+            }
+
+            if (claims.TryGetValue(ClaimTypes.Name, out var userName))
+            {
+                problemDetails.Extensions["userName"] = userName;
+            }
         }
 
         return problemDetails;
