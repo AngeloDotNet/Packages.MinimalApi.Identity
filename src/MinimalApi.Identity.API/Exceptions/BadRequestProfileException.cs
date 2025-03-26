6285a03d9981f12782e97f9da5381537578290bc
Angelo Pirola <angelo9871@gmail.com>
2025-03-26 11:29:44 +0100

    Add custom exception classes for profile errors
    Introduced `BadRequestProfileException` and `NotFoundProfileException` in the `MinimalApi.Identity.API.Exceptions` namespace. Each class extends the base `Exception` class and provides three constructors for flexibility in error handling.

---
+++ src/MinimalApi.Identity.API/Exceptions/BadRequestProfileException.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalApi.Identity.API.Exceptions;
+
+public class BadRequestProfileException : Exception
+{
+    public BadRequestProfileException()
+    { }
+
+    public BadRequestProfileException(string? message) : base(message)
+    { }
+
+    public BadRequestProfileException(string? message, Exception? innerException) : base(message, innerException)
+    { }
+}
