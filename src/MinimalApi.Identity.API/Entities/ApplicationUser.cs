37b22e3a4ea235445150fb351a4f8320c2ae03a3
Angelo Pirola <angelo9871@gmail.com>
2025-03-28 00:02:18 +0100

    Enhance user profile management features
    Uncommented and activated `RefreshToken` and `RefreshTokenExpirationDate` in `ApplicationUser.cs` for better token management. Updated `UserProfile.cs` to include `IsEnabled` and renamed `LastChangePassword` to `LastDateChangePassword` for clarity. Added methods to change user status and validate password change dates.

--- src/MinimalApi.Identity.API/Entities/ApplicationUser.cs
+++ src/MinimalApi.Identity.API/Entities/ApplicationUser.cs
@@ -4,8 +4,8 @@ namespace MinimalApi.Identity.API.Entities;
 
 public class ApplicationUser : IdentityUser<int>
 {
-    //public string RefreshToken { get; set; } = null!;
-    //public DateTime? RefreshTokenExpirationDate { get; set; }
+    public string RefreshToken { get; set; } = null!;
+    public DateTime? RefreshTokenExpirationDate { get; set; }
     public UserProfile UserProfile { get; set; } = null!;
     public ICollection<ApplicationUserRole> UserRoles { get; set; } = [];
     public ICollection<IdentityUserClaim<int>> UserClaims { get; set; } = [];
