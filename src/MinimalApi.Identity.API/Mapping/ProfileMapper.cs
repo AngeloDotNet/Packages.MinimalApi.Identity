8676c9376976ff68882a1a62d735916b60cb4f7e
Angelo Pirola <angelo9871@gmail.com>
2025-04-11 13:12:22 +0200

    Add ProfileMapper class for UserProfile conversion
    Introduces a static class `ProfileMapper` in the
    `MinimalApi.Identity.API.Mapping` namespace. This class
    features an extension method `FromEntity` that transforms
    a `UserProfile` entity into a `UserProfileModel`, utilizing
    properties such as `UserId`, `Email`, `FirstName`,
    `LastName`, `IsEnabled`, and `LastDateChangePassword`.
    Necessary using directives are also added.

---
+++ src/MinimalApi.Identity.API/Mapping/ProfileMapper.cs
@@ -0,0 +1,19 @@
+﻿using MinimalApi.Identity.API.Entities;
+using MinimalApi.Identity.API.Models;
+
+namespace MinimalApi.Identity.API.Mapping;
+
+internal static class ProfileMapper
+{
+    internal static UserProfileModel FromEntity(this UserProfile userProfile)
+    {
+        return new UserProfileModel(
+            userProfile.UserId,
+            userProfile.User.Email!,
+            userProfile.FirstName,
+            userProfile.LastName,
+            userProfile.IsEnabled,
+            userProfile.LastDateChangePassword
+        );
+    }
+}
