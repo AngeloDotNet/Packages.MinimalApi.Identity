f4836b18bed9c979ec7384e493fbd457fde46f01
Angelo Pirola <angelo9871@gmail.com>
2025-04-15 08:10:51 +0200

    Add RefreshTokenValidator for token validation
    Introduces a new `RefreshTokenValidator` class in the
    `MinimalApi.Identity.API.Validator` namespace. This class
    inherits from `AbstractValidator<RefreshTokenModel>` and
    validates that both `AccessToken` and `RefreshToken` are
    not empty, providing appropriate error messages for
    validation failures.

---
+++ src/MinimalApi.Identity.API/Validator/RefreshTokenValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using MinimalApi.Identity.API.Models;
+
+namespace MinimalApi.Identity.API.Validator;
+
+public class RefreshTokenValidator : AbstractValidator<RefreshTokenModel>
+{
+    public RefreshTokenValidator()
+    {
+        RuleFor(x => x.AccessToken)
+            .NotEmpty().WithMessage("Access token is required");
+
+        RuleFor(x => x.RefreshToken)
+            .NotEmpty().WithMessage("Refresh token is required");
+    }
+}
