2424cf693580592c6629f18e0cb6240f35f92594
Angelo Pirola <angelo9871@gmail.com>
2025-04-15 08:10:08 +0200

    Add token refresh endpoint and validation messages
    - Added `EndpointsAuthRefreshToken` to `EndpointsApi` for token refresh functionality.
    - Introduced `InvalidAccessToken` and `InvalidRefreshToken` messages in `MessageApi` for improved error handling.

--- src/MinimalApi.Identity.API/Constants/MessageApi.cs
+++ src/MinimalApi.Identity.API/Constants/MessageApi.cs
@@ -95,4 +95,6 @@ public static class MessageApi
     public const string ProblemDetailsMessageTitle = "An error occurred while processing your request";
     public const string UnexpectedError = "An unexpected error occurred!";
     public const string ConstraintViolation = "A violation occurred for a database constraint";
+    public const string InvalidAccessToken = "Invalid access token";
+    public const string InvalidRefreshToken = "Invalid refresh token";
 }
