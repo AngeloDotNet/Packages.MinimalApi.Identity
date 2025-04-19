bbb8e85328cbf2217403f6b676135ff4e135b43c
Angelo Pirola <angelo9871@gmail.com>
2025-04-19 09:00:59 +0200

    Refactor user-related methods into UsersExtensions
    Updated using directives in ServicesExtensions.cs to include necessary namespaces for dependency injection. Removed the `GetUserId` and `GetClaimValue` methods from `ServicesExtensions` and moved them to a newly created `UsersExtensions` class. Added a new `IsAuthenticated` method to enhance user authentication checks, throwing a `UserUnknownException` if the user is not authenticated.

---
+++ src/MinimalApi.Identity.API/Extensions/UsersExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using MinimalApi.Identity.API.Exceptions.Users;
+
+namespace MinimalApi.Identity.API.Extensions;
+
+public static class UsersExtensions
+{
+    public static string GetUserId(this IPrincipal user)
+    {
+        if (user is not ClaimsPrincipal claimsPrincipal)
+        {
+            throw new ArgumentException("User must be a ClaimsPrincipal", nameof(user));
+        }
+
+        var claimValue = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (string.IsNullOrEmpty(claimValue))
+        {
+            throw new InvalidOperationException("Claim value for NameIdentifier is missing or empty.");
+        }
+
+        return claimValue;
+    }
+
+    public static string GetClaimValue(this IPrincipal user, string claimType) => ((ClaimsPrincipal)user).FindFirst(claimType)?.Value!;
+
+    public static bool IsAuthenticated(this ClaimsPrincipal principal)
+    {
+        var userIsAuthenticated = principal.Identity?.IsAuthenticated ?? false;
+
+        if (userIsAuthenticated == false)
+        {
+            throw new UserUnknownException("User is not authenticated");
+        }
+
+        return userIsAuthenticated;
+    }
+}
