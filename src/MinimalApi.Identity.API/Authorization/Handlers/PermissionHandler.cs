6c39c8a27d6f7b93b03765886100c6d14253a54e
Angelo Pirola <angelo9871@gmail.com>
2025-04-19 09:00:48 +0200

    Refactor PermissionHandler to simplify authentication checks
    Comment out existing user authentication and permission validation logic in the PermissionHandler class. The new implementation focuses on user authentication and retrieval of user details, while detailed permission checks and exception handling are left for future cleanup. This change simplifies the flow of the HandleAsync method but removes critical security checks.

--- src/MinimalApi.Identity.API/Authorization/Handlers/PermissionHandler.cs
+++ src/MinimalApi.Identity.API/Authorization/Handlers/PermissionHandler.cs
@@ -10,6 +10,7 @@ using MinimalApi.Identity.BusinessLayer.Authorization.Requirement;
 
 namespace MinimalApi.Identity.API.Authorization.Handlers;
 
+//TODO: cleanup this class
 public class PermissionHandler(ILogger<PermissionHandler> logger, UserManager<ApplicationUser> userManager) : IAuthorizationHandler
 {
     public async Task HandleAsync(AuthorizationHandlerContext context)
@@ -17,53 +18,103 @@ public class PermissionHandler(ILogger<PermissionHandler> logger, UserManager<Ap
         var user = context.User;
         var permissionsRequirements = context.Requirements.OfType<PermissionRequirement>();
 
-        if (user.Identity?.IsAuthenticated != true)
-        {
-            var message = "User is not authenticated";
+        //if (user.Identity?.IsAuthenticated != true)
+        //{
+        //    var message = "User is not authenticated";
 
-            logger.LogWarning(message);
-            throw new UserUnknownException(message);
-        }
+        //    logger.LogWarning(message);
+        //    throw new UserUnknownException(message);
+        //}
 
-        foreach (var permissionRequirement in permissionsRequirements)
+        //if (UsersExtensions.IsAuthenticated(user, logger))
+        //{
+        //    foreach (var permissionRequirement in permissionsRequirements)
+        //    {
+        //        if (permissionRequirement.Permissions.All(permission => user.HasClaim(CustomClaimTypes.Permission, permission)))
+        //        {
+        //            context.Succeed(permissionRequirement);
+        //        }
+        //        else
+        //        {
+        //            var message = $"User {user?.Identity?.Name} does not have the required permissions";
+
+        //            logger.LogWarning(message);
+        //            throw new UserWithoutPermissionsException(message);
+        //        }
+        //    }
+
+        //    var userId = user.GetUserId();
+        //    var utente = await userManager.FindByIdAsync(userId);
+        //    var securityStamp = context.User.GetClaimValue(ClaimTypes.SerialNumber);
+
+        //    if (utente == null)
+        //    {
+        //        var message = $"User {user?.Identity?.Name} not found";
+
+        //        logger.LogWarning(message);
+        //        throw new UserUnknownException(message);
+        //    }
+        //    else if (utente.LockoutEnd.GetValueOrDefault() > DateTimeOffset.UtcNow)
+        //    {
+        //        var message = MessageApi.UserLockedOut;
+
+        //        logger.LogWarning(message);
+        //        throw new UserIsLockedException(message);
+        //    }
+        //    else if (securityStamp != utente.SecurityStamp)
+        //    {
+        //        var message = $"User {user?.Identity?.Name} security stamp is invalid";
+
+        //        logger.LogWarning(message);
+        //        throw new UserTokenIsInvalidException(message);
+        //    }
+        //}
+
+        if (UsersExtensions.IsAuthenticated(user))
         {
-            if (permissionRequirement.Permissions.All(permission => user.HasClaim(CustomClaimTypes.Permission, permission)))
+            var userId = user.GetUserId();
+            var nameUser = user?.Identity?.Name;
+            var securityStamp = context.User.GetClaimValue(ClaimTypes.SerialNumber);
+            var utente = await userManager.FindByIdAsync(userId);
+
+            if (utente == null || user == null)
             {
-                context.Succeed(permissionRequirement);
-            }
-            else
-            {
-                var message = $"User {user?.Identity?.Name} does not have the required permissions";
+                var message = $"User {nameUser} not found";
 
                 logger.LogWarning(message);
-                throw new UserWithoutPermissionsException(message);
+                throw new UserUnknownException(message);
             }
-        }
 
-        var userId = user.GetUserId();
-        var utente = await userManager.FindByIdAsync(userId);
-        var securityStamp = context.User.GetClaimValue(ClaimTypes.SerialNumber);
+            if (utente.LockoutEnd.GetValueOrDefault() > DateTimeOffset.UtcNow)
+            {
+                var message = MessageApi.UserLockedOut;
 
-        if (utente == null)
-        {
-            var message = $"User {user?.Identity?.Name} not found";
+                logger.LogWarning(message);
+                throw new UserIsLockedException(message);
+            }
 
-            logger.LogWarning(message);
-            throw new UserUnknownException(message);
-        }
-        else if (utente.LockoutEnd.GetValueOrDefault() > DateTimeOffset.UtcNow)
-        {
-            var message = MessageApi.UserLockedOut;
+            if (securityStamp != utente.SecurityStamp)
+            {
+                var message = $"User {nameUser} security stamp is invalid";
 
-            logger.LogWarning(message);
-            throw new UserIsLockedException(message);
-        }
-        else if (securityStamp != utente.SecurityStamp)
-        {
-            var message = $"User {user?.Identity?.Name} security stamp is invalid";
-            logger.LogWarning(message);
+                logger.LogWarning(message);
+                throw new UserTokenIsInvalidException(message);
+            }
 
-            throw new UserTokenIsInvalidException(message);
+            foreach (var permissionRequirement in permissionsRequirements)
+            {
+                if (permissionRequirement.Permissions.All(permission => user.HasClaim(CustomClaimTypes.Permission, permission)))
+                {
+                    context.Succeed(permissionRequirement);
+                }
+                else
+                {
+                    var message = $"User {nameUser} does not have the required permissions";
+
+                    logger.LogWarning(message);
+                    throw new UserWithoutPermissionsException(message);
+                }
+            }
         }
 
         await Task.CompletedTask;
